45f1d9bbc06ad9e5d53cea6fe95b94d25c88fd63
ymartinguzman <yamiramartinguzman@gmail.com>
2020-10-19 18:37:12 +0200

    add form action in html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const writeNum = document.querySelector(".js__write-num");
 const buttonTry = document.querySelector(".js__btn");
@@ -6,27 +6,26 @@ const trial = document.querySelector(".js__trial");
 const trialNum = document.querySelector(".js__trial-num");
 const randomNum = getRandomNumber(100);
 
-function magicNumber(){
-  const writeNumValue = writeNum.value;
-if(writeNumValue < 1 || writeNumValue> 100 ){
-  console.log("El número debe estar entre 1 y 100");
-  trial.value =("El número debe estar entre 1 y 100");
-} else if(randomNum > writeNumValue){
-  console.log("Demasiado bajo");
-  trial.value = "Demasiado bajo";
-}else if(randomNum < writeNumValue){
-  console.log("Demasiado alto");
- trial.value =("Demasiado alto");
-}else if(randomNum === writeNumValue){
-  console.log("Has ganado campeona");
-  trial.value =("Has ganado campeona");
-}
+function magicNumber() {
+  const writeNumValue = parseInt(writeNum.value);
+  if (writeNumValue < 1 || writeNumValue > 100) {
+    console.log("El número debe estar entre 1 y 100");
+    trial.value = "El número debe estar entre 1 y 100";
+  } else if (randomNum > writeNumValue) {
+    console.log("Demasiado bajo");
+    trial.value = "Demasiado bajo";
+  } else if (randomNum < writeNumValue) {
+    console.log("Demasiado alto");
+    trial.value = "Demasiado alto";
+  } else if (randomNum === writeNumValue) {
+    console.log("Has ganado campeona");
+    trial.value = "Has ganado campeona";
+  }
 }
 
 function getRandomNumber(max) {
- return Math.ceil(Math.random() * max);
+  return Math.ceil(Math.random() * max);
 }
-
 
 console.log(getRandomNumber(100));
 
