eaf4f4d5a53b4085f4d9e9ec971db31fcd6f9dd4
ymartinguzman <yamiramartinguzman@gmail.com>
2020-10-21 20:16:52 +0200

    add counter function

--- js/main.js
+++ js/main.js
@@ -1,32 +1,50 @@
-"use strict";
+'use strict';
 
-const writeNum = document.querySelector(".js__write-num");
-const buttonTry = document.querySelector(".js__btn");
-const trial = document.querySelector(".js__trial");
-const trialNum = document.querySelector(".js__trial-num");
+const writeNum = document.querySelector('.js__write-num');
+const buttonTry = document.querySelector('.js__btn');
+const trial = document.querySelector('.js__trial');
+const trialNum = document.querySelector('.js__trial-num');
 const randomNum = getRandomNumber(100);
 
 function magicNumber() {
-  const writeNumValue = parseInt(writeNum.value);
+  let writeNumValue = parseInt(writeNum.value);
+
   if (writeNumValue < 1 || writeNumValue > 100) {
-    console.log("El número debe estar entre 1 y 100");
-    trial.value = "El número debe estar entre 1 y 100";
+    console.log('El número debe estar entre 1 y 100');
+    trial.value = 'El número debe estar entre 1 y 100';
   } else if (randomNum > writeNumValue) {
-    console.log("Demasiado bajo");
-    trial.value = "Demasiado bajo";
+    console.log('Demasiado bajo');
+    trial.value = 'Demasiado bajo';
   } else if (randomNum < writeNumValue) {
-    console.log("Demasiado alto");
-    trial.value = "Demasiado alto";
+    console.log('Demasiado alto');
+    trial.value = 'Demasiado alto';
   } else if (randomNum === writeNumValue) {
-    console.log("Has ganado campeona");
-    trial.value = "Has ganado campeona";
+    console.log('Has ganado campeona');
+    trial.value = 'Has ganado campeona';
   }
 }
+
+let accCounter = 0;
+
+function counter() {
+  if (trial.value === 'Has ganado campeona') {
+    accCounter += 0;
+    trialNum.value = 'Número de intentos: ' + accCounter;
+  } else {
+    accCounter += 1;
+    trialNum.value = 'Número de intentos: ' + accCounter;
+  }
+}
+
+function lastTrial() {
+  magicNumber();
+  counter();
+}
+
+console.log(accCounter);
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-
-console.log(getRandomNumber(100));
-
-buttonTry.addEventListener("click", magicNumber);
+console.log(randomNum);
+buttonTry.addEventListener('click', lastTrial);
