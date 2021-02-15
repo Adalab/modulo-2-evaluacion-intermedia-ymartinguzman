44225635dd9da28db2c765d49e647ab4ca7e06d0
ymartinguzman <yamiramartinguzman@gmail.com>
2021-02-15 11:56:37 +0100

    add button for new game and change display

--- js/main.js
+++ js/main.js
@@ -5,22 +5,23 @@ const buttonTry = document.querySelector('.js__btn');
 const trial = document.querySelector('.js__trial');
 const trialNum = document.querySelector('.js__trial-num');
 const randomNum = getRandomNumber(100);
+const newGame = document.querySelector('.js__newGame');
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
 
 function magicNumber() {
   let writeNumValue = parseInt(writeNum.value);
 
   if (writeNumValue < 1 || writeNumValue > 100) {
-    console.log('El número debe estar entre 1 y 100');
     trial.value = 'El número debe estar entre 1 y 100';
+  } else if (randomNum === writeNumValue) {
+    trial.value = '¡Has adivinado el número!';
   } else if (randomNum > writeNumValue) {
-    console.log('Demasiado bajo');
     trial.value = 'Demasiado bajo';
   } else if (randomNum < writeNumValue) {
-    console.log('Demasiado alto');
     trial.value = 'Demasiado alto';
-  } else if (randomNum === writeNumValue) {
-    console.log('Has ganado campeona');
-    trial.value = 'Has ganado campeona';
   }
 }
 
@@ -36,15 +37,14 @@ function counter() {
   }
 }
 
-function lastTrial() {
+function Trial() {
   magicNumber();
   counter();
 }
 
-console.log(accCounter);
-
-function getRandomNumber(max) {
-  return Math.ceil(Math.random() * max);
+function newNumber() {
+  magicNumber();
 }
-console.log(randomNum);
-buttonTry.addEventListener('click', lastTrial);
+
+buttonTry.addEventListener('click', Trial);
+newGame.addEventListener('click', newNumber);
